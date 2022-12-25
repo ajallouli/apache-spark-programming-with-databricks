9c6a43ebadedb05fd47449e4e7a18c8283a83a9f
amine.jallouli@outlook.com <amine.jallouli@outlook.com>
2022-12-25 14:33:16 +0000

    02-mounting-storage-container-with-sas-token

--- Extra/01 - Mount a storage to Databricks.py
+++ Extra/01 - Mount a storage to Databricks.py
@@ -39,6 +39,10 @@
 
 # COMMAND ----------
 
+# MAGIC %md ### 4. Mounting a storage container through an account key
+
+# COMMAND ----------
+
 dbutils.fs.mount(
     source='wasbs://data@samydatalake.blob.core.windows.net',
     mount_point='/mnt/aminedata',
@@ -53,3 +57,47 @@ display(dbutils.fs.ls('/mnt/aminedata'))
 
 # MAGIC %sh
 # MAGIC ls -al
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### 5. Creating another container
+# MAGIC 
+# MAGIC ```sh
+# MAGIC az storage container create \
+# MAGIC   --name sasdata \
+# MAGIC   --account-name samydatalake
+# MAGIC ```
+# MAGIC 
+# MAGIC and then, create a SAS token throught Azure Portal.
+# MAGIC 
+# MAGIC The SAS token is `sp=racwdli&st=2022-12-25T13:57:59Z&se=2022-12-25T21:57:59Z&spr=https&sv=2021-06-08&sr=c&sig=6%2FpFlbUWx0vUDS2xqhkiG12xzqAk6aBxdqlOYqPrNwY%3D`
+
+# COMMAND ----------
+
+# MAGIC %md 
+# MAGIC ### 6. Mounting storage container throught a SAS token
+
+# COMMAND ----------
+
+dbutils.fs.mount(
+    source='wasbs://sasdata@samydatalake.blob.core.windows.net',
+    mount_point='/mnt/sasdata',
+    extra_configs={'fs.azure.sas.sasdata.samydatalake.blob.core.windows.net':'sp=racwdli&st=2022-12-25T13:57:59Z&se=2022-12-25T21:57:59Z&spr=https&sv=2021-06-08&sr=c&sig=6%2FpFlbUWx0vUDS2xqhkiG12xzqAk6aBxdqlOYqPrNwY%3D'}
+)
+
+# COMMAND ----------
+
+display(dbutils.fs.ls('/mnt/sasdata'))
+
+# COMMAND ----------
+
+# MAGIC %md ### 7. Listing the mounts to databricks
+
+# COMMAND ----------
+
+display(dbutils.fs.mounts())
+
+# COMMAND ----------
+
+
